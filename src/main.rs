hidutils/hid-recorder

commit 680fddab67b1667fb02c00addde9747f726b3cb1

Shorten repeat usages
Wacom HID descriptors in particular have things like this

   # 0x85, 0xe4,                    //   Report ID (228)                         939
   # 0x09, 0x01,                    //   Usage (Wacom Digitizer)                 941
   # 0x96, 0xff, 0x01,              //   Report Count (511)                      943
   # 0xb1, 0x02,                    //   Feature (Data,Var,Abs)                  946

i.e. 512 bytes with the same usage. Printing those one-by-one makes the
output impossible to navigate and it provides no useful information
anyway. Shorten this like the vendor output so we now print this:

  ------- Feature Report -------
  Report ID: 228
     Report size: 4096 bits
   | Bits:   8..=15  | Usage: ff0d/0001: Wacom / Wacom Digitizer                     | Logical Range:     0..=255   | Physical Range:     0..=359
   | Bits:  16..=23  | Usage: ff0d/0001: Wacom / Wacom Digitizer                     | Logical Range:     0..=255   | Physical Range:     0..=359
   | Bits:  24..=31  | Usage: ff0d/0001: Wacom / Wacom Digitizer                     | Logical Range:     0..=255   | Physical Range:     0..=359
   | Bits:  32..=39  | Usage: ff0d/0001: Wacom / Wacom Digitizer                     | Logical Range:     0..=255   | Physical Range:     0..=359
   |                 | Total of 511 repeated usages, ... use --full to see all

src/main.rs
@@ -544,6 +544,20 @@ fn vendor_report_filler(count: usize) -> PrintableRow {
     }
 }
 
+fn repeat_usage_filler(count: usize) -> PrintableRow {
+    PrintableRow {
+        bits: PrintableColumn::from("  "),
+        usage: PrintableColumn {
+            string: format!(
+                "Total of {} repeated usages, ... use --full to see all",
+                count + 1
+            ),
+            style: Styles::Note,
+        },
+        ..Default::default()
+    }
+}
+
 /// Print the parsed reports as an outline of how they look like
 fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options) {
     if r.report_id().is_some() {
@@ -563,6 +577,8 @@ fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options
 
     const REPEAT_LIMIT: usize = 3;
 
+    let mut last_usage: Usage = Usage::from(0);
+    let mut repeat_usage_count = 0;
     let mut vendor_report_count = 0;
     let mut table = PrintableTable::default();
     for field in r.fields() {
@@ -578,10 +594,13 @@ fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options
         }
         match field {
             Field::Constant(_c) => {
+                if repeat_usage_count > REPEAT_LIMIT {
+                    table.add(repeat_usage_filler(repeat_usage_count));
+                }
                 row.usage = "######### Padding".into();
             }
             Field::Variable(v) => {
-                if vendor_report_count <= REPEAT_LIMIT {
+                if vendor_report_count <= REPEAT_LIMIT && repeat_usage_count <= REPEAT_LIMIT {
                     row.usage = format!("Usage: {}", usage_to_str(&v.usage)).into();
                     row.logical_range =
                         logical_range_to_str(&v.logical_minimum, &v.logical_maximum).into();
@@ -589,8 +608,20 @@ fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options
                         physical_range_to_str(&v.physical_minimum, &v.physical_maximum).into();
                     row.unit = unit_to_str(&v.unit).into();
                 }
+                if !opts.full && last_usage == v.usage {
+                    repeat_usage_count += 1;
+                } else {
+                    if repeat_usage_count > REPEAT_LIMIT {
+                        table.add(repeat_usage_filler(repeat_usage_count));
+                    }
+                    repeat_usage_count = 0;
+                }
+                last_usage = v.usage;
             }
             Field::Array(a) => {
+                if repeat_usage_count > REPEAT_LIMIT {
+                    table.add(repeat_usage_filler(repeat_usage_count));
+                }
                 row.usage = "Usages:".into();
                 row.logical_range =
                     logical_range_to_str(&a.logical_minimum, &a.logical_maximum).into();
@@ -626,13 +657,16 @@ fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options
                 }
             }
         }
-        if vendor_report_count <= REPEAT_LIMIT {
+        if vendor_report_count <= REPEAT_LIMIT && repeat_usage_count <= REPEAT_LIMIT {
             table.add(row);
         }
     }
 
     if vendor_report_count > REPEAT_LIMIT {
         table.add(vendor_report_filler(vendor_report_count));
+    }
+    if repeat_usage_count > REPEAT_LIMIT {
+        table.add(repeat_usage_filler(repeat_usage_count));
     }
     for row in table.rows {
         cprint!(stream, Styles::None, "#  ");

commit e656c79fd76a8123cd72ef4d7cc0f92b2e3893c7

Only print one of the vendor or repeat usages message
Some devices have repeated identical vendor usages, let's only print one
message in that case.

Fixes: 680fddab67b1 ("Shorten repeat usages")

src/main.rs
@@ -685,8 +685,7 @@ fn print_report_summary(stream: &mut impl Write, r: &impl Report, opts: &Options
 
     if vendor_report_count > REPEAT_LIMIT {
         table.add(vendor_report_filler(vendor_report_count));
-    }
-    if repeat_usage_count > REPEAT_LIMIT {
+    } else if repeat_usage_count > REPEAT_LIMIT {
         table.add(repeat_usage_filler(repeat_usage_count));
     }
     for row in table.rows {