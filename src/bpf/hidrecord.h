6788076f12745609dd790e6cae05d45666e2f252
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2024-07-18 14:16:40 +0200

    Add support for pre-6.11 kernel for the bpf logging
    Those kernel do not support struct_ops, and thus need a special bpf
    program and attach logic.

---
+++ src/bpf/hidrecord.h
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2024 Benjamin Tissoires
+ */
+#include "vmlinux.h"
+#include <bpf/bpf_tracing.h>
+
+char LICENSE[] SEC("license") = "GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 256 * 1024);
+} events SEC(".maps");
+
+struct hid_recorder_event {
+	__u8 length;
+	__u8 data[64];
+};
+
+/* following are kfuncs exported by HID for HID-BPF */
+extern __u8 *hid_bpf_get_data(struct hid_bpf_ctx *ctx,
+			      unsigned int offset,
+			      const size_t __sz) __ksym;
+
+int process_event(struct hid_bpf_ctx *hctx)
+{
+	__u8 *data = hid_bpf_get_data(hctx, 0 /* offset */, 64 /* size */);
+	struct hid_recorder_event *event;
+
+	if (!data)
+		return 0; /* EPERM check */
+
+	event = bpf_ringbuf_reserve(&events, sizeof(*event), 0);
+	if (!event)
+		return 0;
+
+	event->length = hctx->size;
+	__builtin_memcpy(event->data, data, sizeof(event->data));
+
+	bpf_ringbuf_submit(event, 0);
+
+	return 0;
+}
