aa6e951ca078eb5990c9ecab0a3c45f387e60f3b
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2024-07-19 15:02:45 +0200

    Conditionally recompile the bpf if clang is available
    if clang is not available, we use the provided *.skel.rs.

--- build.rs
+++ build.rs
@@ -1,18 +1,41 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use libbpf_cargo::SkeletonBuilder;
 
 const SRC: &str = "src/bpf/hidrecord.bpf.c";
 const SRC_TRACING: &str = "src/bpf/hidrecord_tracing.bpf.c";
 
+// from https://stackoverflow.com/questions/37498864/finding-executable-in-path-with-rust
+fn which<P>(exe_name: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
+    env::var_os("PATH").and_then(|paths| {
+        env::split_paths(&paths)
+            .filter_map(|dir| {
+                let full_path = dir.join(&exe_name);
+                if full_path.is_file() {
+                    Some(full_path)
+                } else {
+                    None
+                }
+            })
+            .next()
+    })
+}
+
 fn main() {
-    let mut out = PathBuf::from(
-        env::var_os("OUT_DIR").expect("expected OUT_DIR to be set by cargo but it's empty"),
-    );
-    let mut out_tracing = out.clone();
-    out.push("hidrecord.skel.rs");
-    out_tracing.push("hidrecord_tracing.skel.rs");
+    if which("clang").is_none() {
+        println!(
+            "cargo:warning='clang' executable not found, using provided bpf compilation outputs"
+        );
+        println!("cargo:warning= changes in 'src/bpf/*' will not be taken into account");
+        return;
+    }
+
+    let out = PathBuf::from("src/bpf/hidrecord.skel.rs");
+    let out_tracing = PathBuf::from("src/bpf/hidrecord_tracing.skel.rs");
 
     SkeletonBuilder::new()
         .source(SRC)
--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,10 @@ use libbpf_rs::skel::OpenSkel as _;
 use libbpf_rs::skel::SkelBuilder as _;
 
 mod hidrecord {
-    include!(concat!(env!("OUT_DIR"), "/hidrecord.skel.rs"));
+    include!("bpf/hidrecord.skel.rs");
 }
 mod hidrecord_tracing {
-    include!(concat!(env!("OUT_DIR"), "/hidrecord_tracing.skel.rs"));
+    include!("bpf/hidrecord_tracing.skel.rs");
 }
 
 use hidrecord::*;
