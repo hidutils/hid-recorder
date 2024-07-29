ddc150c9f3182e28b9429123042ad104cb8d9693
Peter Hutterer <peter.hutterer@who-t.net>
2024-07-29 11:11:07 +1000

    build.rs: don't re-implement which, try to compile instead
    Instead of reimplementing which let's just try to compile things and
    fall back to our BPFs if the compilation fails. This is more robust than
    trying re-create whatever libbpf_cargo does in its clang invocation.
    
    Unfortunately the error itself is just a anyhow::bail so we can't detect
    a specific "compiler is missing" error unless we want to dig around in
    the error messages.

--- build.rs
+++ build.rs
@@ -1,46 +1,18 @@
-use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use libbpf_cargo::SkeletonBuilder;
 
 const SRC: &str = "src/bpf/hidrecord.bpf.c";
 const SRC_TRACING: &str = "src/bpf/hidrecord_tracing.bpf.c";
 
-// from https://stackoverflow.com/questions/37498864/finding-executable-in-path-with-rust
-fn which<P>(exe_name: P) -> Option<PathBuf>
-where
-    P: AsRef<Path>,
-{
-    env::var_os("PATH").and_then(|paths| {
-        env::split_paths(&paths)
-            .filter_map(|dir| {
-                let full_path = dir.join(&exe_name);
-                if full_path.is_file() {
-                    Some(full_path)
-                } else {
-                    None
-                }
-            })
-            .next()
-    })
-}
-
 fn main() {
-    if which("clang").is_none() {
-        println!(
-            "cargo:warning='clang' executable not found, using provided bpf compilation outputs"
-        );
-        println!("cargo:warning= changes in 'src/bpf/*' will not be taken into account");
-        return;
-    }
-
     let out = PathBuf::from("src/bpf/hidrecord.skel.rs");
     let out_tracing = PathBuf::from("src/bpf/hidrecord_tracing.skel.rs");
 
-    SkeletonBuilder::new()
-        .source(SRC)
-        .build_and_generate(out)
-        .expect("bpf compilation failed");
+    if let Err(e) = SkeletonBuilder::new().source(SRC).build_and_generate(out) {
+        println!("cargo:warning=Failed to build BPF sources: {:?}. Using provided BPF compilation outputs instead.", e.source().unwrap());
+        return;
+    }
     println!("cargo:rerun-if-changed={}", SRC);
     SkeletonBuilder::new()
         .source(SRC_TRACING)
